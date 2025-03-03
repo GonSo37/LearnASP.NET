d1a2bcf5e9dcc01f8e2dab719acc2d47b571b3d3
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-03-03 12:03:10 +0100

    first commit

---
+++ LearningASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearningASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
