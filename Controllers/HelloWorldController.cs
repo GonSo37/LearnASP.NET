e310f3748a9f9cb655f3cf8e66bcf6cda10c5348
Vladyslav Tarasov <tarasovvlad137@gmail.com>
2025-03-05 08:34:02 +0100

    Updated repository structure

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+
+namespace LearningASPNET.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public string Welcome(string Name, int Age)
+    {
+        return HtmlEncoder.Default.Encode($"Hello {Name}, your age is {Age}");
+    }
+}
+
